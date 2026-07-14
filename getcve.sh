81f68a593bb0821bcaf620495945e82fe4803355
David Lamparter <equinox@diac24.net>
2026-07-15 00:24:36 +0200

    improve getcve.sh
    Signed-off-by: David Lamparter <equinox@diac24.net>

--- getcve.sh
+++ getcve.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-for D in CVE-*; do
-	test -d "$D" || continue
-	test -e "$D/cve.json" && continue
+test "$#" -eq 0 && set -- CVE-*
 
-	wget -O "$D/cve.json" "https://cveawg.mitre.org/api/cve/$D"
-	jq < "$D/cve.json" > "$D/cve-pretty.json"
+for D in "$@"; do
+	test -e "$D" || mkdir "$D"
+	test \! -d "$D" && { echo "not a directory: $D" >&2; continue; }
+
+	echo "$D" >&2
+	wget -O - "https://cveawg.mitre.org/api/cve/$D" | jq > "$D/cve.json"
 done
